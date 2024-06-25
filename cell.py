7ca96770ab2ef8a2c8309355479f00543f57c46d
Igor Dias Aguiar <157843652+igorgynIA@users.noreply.github.com>
2024-06-25 17:55:23 -0300

    Update cell.py

--- cell.py
+++ cell.py
@@ -1,12 +1,7 @@
 import cv2
-import math 
-
-# Classes de objetos (neste caso, apenas Coca-Cola)
-classNames = ["Coca-Cola"]
-
 
 # Substitua pelo endereço IP fornecido pelo IP Webcam, seguido de "/video"
-url = "http://192.168.0.236:4747/video"
+url = "http://IP:PORT/video"
 
 # Abra a câmera
 cap = cv2.VideoCapture(url)
@@ -23,28 +18,3 @@ while True:
 
 cap.release()
 cv2.destroyAllWindows()
-
-#Carregar modelo
-model = "C:\\Igor\\best.pt"
-
-# Função para realizar a detecção de objetos em um frame
-def detect_objects(frame, model):
-    results = model(frame, stream=True)
-    detections = []
-    for r in results:
-        boxes = r.boxes
-        for box in boxes:
-            x1, y1, x2, y2 = box.xyxy[0]
-            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-            confidence = math.ceil((box.conf[0] * 100)) / 100
-            cls = int(box.cls[0])
-            label = classNames[cls]
-            detections.append((label, confidence, (x1, y1, x2, y2)))
-    return detections
-
-# Função para exibir os resultados de detecção no frame
-def draw_detections(frame, detections):
-    for label, confidence, (x1, y1, x2, y2) in detections:
-        cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 255), 3)
-        cv2.putText(frame, f'{label} {confidence}', (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 2)
-    return frame
